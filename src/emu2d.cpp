056cd02cbf79a41b5bbf635785d4e0d7832e9c25
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-05-07 11:45:57 +0100

    .

--- src/emu2d.cpp
+++ src/emu2d.cpp
@@ -1,5 +1,4 @@
 # include "emu2d.hpp"
-
-mdl::uint_t mdl::emu2d(uint_t __xa, uint_t __ya, uint_t __xa_len, uint_t __ya_len, uint_t __offset) {
-    return ((__xa + (__ya * __xa_len)) * __offset);
+mdl::uint_t mdl::emu2d(uint_t __xa, uint_t __ya, uint_t __xa_len, uint_t __ya_len, uint_t __fs) {
+    return ((__xa + (__ya * __xa_len)) * __fs);
 }
