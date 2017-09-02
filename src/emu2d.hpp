1d7a022fa700519fc295c1f7e2214f2d880fdf1e
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-09-02 15:40:09 +0100

    .

--- src/emu2d.hpp
+++ src/emu2d.hpp
@@ -1,6 +1,6 @@
 # ifndef __emu2d__hpp
 # define __emu2d__hpp
-# include <eint_t.hpp>
+# include <mdlint.h>
 namespace mdl {
     uint_t emu2d(uint_t __xa, uint_t __ya, uint_t __xa_len, uint_t __ya_len, uint_t __fs);
 }
